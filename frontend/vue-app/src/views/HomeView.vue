c67899ce5270cb682b35e0e83dc9086bd4c9ab71
Huiju Lee <leemoon63@gmail.com>
2024-01-23 00:05:32 +0900

    build: axous 연결 테스트

--- frontend/vue-app/src/views/HomeView.vue
+++ frontend/vue-app/src/views/HomeView.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="home">
     <h1>안녕하세요 속초영성교회입니다.</h1>
-    </div>
+    <button @click="getData">데이터 호출</button>  
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  methods: {
+    getData() {
+      axios.get('http://localhost:8080')
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
 }
 </script>
