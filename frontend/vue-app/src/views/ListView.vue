46a8b8bbf4b313be1a1c50aa10a803daaf21cc0c
Huiju Lee <leemoon63@gmail.com>
2024-02-05 23:17:32 +0900

    feat: memberList front view 구현

--- frontend/vue-app/src/views/ListView.vue
+++ frontend/vue-app/src/views/ListView.vue
@@ -1,43 +1,68 @@
 <template>
-    <div class="container mt-3">
-          <h1 class="display-1 text-center">회원 목록</h1>
-          <div class="btn-group">
-            <a href="/member/save" class="btn btn-primary">회원 추가</a>
-          </div>
-          <table class="table table-hover mt-3">
-            <thead class="table-dark">
-              <tr>
-                <th>이름</th>
-                <th>이메일</th>
-                <th>성별</th>
-                <th>등록여부</th>
-                <th>가입일</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="cursor-pointer" onclick="location.href = '/member/findById';">
-                <td>스티븐</td>
-                <td>jobs@shellfolder.com</td>
-                <td>남자</td>
-                <td>O</td>
-                <td>2023-02-28</td>
-              </tr>
-              <tr class="cursor-pointer" onclick="location.href = '/member/findById';">
-                <td>에브릴</td>
-                <td>lavigne@shellfolder.com</td>
-                <td>여자</td>
-                <td>X</td>
-                <td>2023-02-27</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-    </template>
+  <div class="container mt-3">
+    <h1 class="display-1 text-center">회원 목록</h1>
+    <div class="btn-group">
+      <a href="/member/save" class="btn btn-primary">회원 추가</a>
+    </div>
+    <table class="table table-hover mt-3">
+      <thead class="table-dark">
+        <tr>
+          <th>이름</th>
+          <th>이메일</th>
+          <th>성별</th>
+          <th>등록여부</th>
+          <th>가입일</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="cursor-pointer" v-for="member in memberList" :key="member.id"
+        @click="$event => href(member)">
+          <td>{{ member.name }}</td>
+          <td>{{ member.email }}</td>
+          <td>{{ member.sex }}</td>
+          <td>{{ member.isChurchMember }}</td>
+          <td>{{ member.createdTime }}</td>
+        </tr>
+
+      </tbody>
+    </table>
+  </div>
+</template>
     
-    <script>
-    </script>
-    <style scoped>
-    tbody > tr {
-        cursor: pointer;
+<script>
+import axios from 'axios'
+export default {
+  name: "ListView",
+  data() {
+    return {
+      memberList: []
     }
-    </style>
+  }, 
+  created() {
+    this.getMemberList()
+  },
+  methods: {
+    getMemberList() {
+      axios
+        .get('http://localhost:8080/admin/members')
+        .then((response) => {
+          console.log(response)
+          this.memberList = response.data.result
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    href(member) {
+      console.log(member)
+      this.$router.push({name: "SelectView"})
+    }
+  }
+}
+
+</script>
+<style scoped>
+tbody>tr {
+  cursor: pointer;
+}
+</style>
