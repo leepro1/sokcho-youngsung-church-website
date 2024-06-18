7222375df748ffca373d8abe346740b29dad473f
Huiju <leemoon63@gmail.com>
2024-06-18 17:18:10 +0900

    sokchoys 프로젝트 초기화

---
+++ sokchoys/src/main/java/com/sokchoys/SokchoysApplication.java
@@ -0,0 +1,13 @@
+package com.sokchoys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokchoysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SokchoysApplication.class, args);
+	}
+
+}
---
+++ sokchoys/src/test/java/com/sokchoys/SokchoysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sokchoys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SokchoysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
